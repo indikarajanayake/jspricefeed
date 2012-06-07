1e074f646f4a40d144f2ac41b4a14b7a3d95f1b8
Indika Rajanayake <indikarajanayake@gmail.com>
2012-06-07 23:05:54 +1000

    change port to heroku

--- app.js
+++ app.js
@@ -55,6 +55,6 @@ var priceProvider= new PriceProvider();
 
 
 
-app.listen(3000, function(){
+app.listen(27040, function(){
   console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 });
